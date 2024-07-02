bfa67efb7c978984e8529870bbd44eeed08b0ece
maruf-ahmed <marufahamed1110@gmail.com>
2024-07-02 11:21:33 +0600

    demo commit

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,57 @@
+"use client";
+import Image from "next/image";
+import logo from "../../asset/imgs/logo.png";
+import Link from "next/link";
+import { AiOutlineClose } from "react-icons/ai";
+import { AiOutlineBars } from "react-icons/ai";
+import { useState } from "react";
+
+function Navbar() {
+  const [showDropMenu, setShowDropMenu] = useState(false);
+
+  return (
+    <div className="bg-base-100">
+      <div className="navbar max-w-7xl mx-auto">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <div onClick={() => setShowDropMenu(!showDropMenu)} className="">
+                {
+                  showDropMenu?  <AiOutlineClose className='text-xl' /> : <AiOutlineBars className='text-xl' />
+                }
+               
+              </div>
+            </div>
+          </div>
+          <div className="">
+            <Image src={logo} alt="logo-img" height={250} width={250}></Image>
+          </div>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal gap-4 px-1">
+            <Link href="/">Home</Link>
+            <Link href="/">About</Link>
+            <Link href="/">Appointment</Link>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Button</a>
+        </div>
+      </div>
+
+      <div
+        className={
+          showDropMenu
+            ? "flex flex-col absolute  gap-3 p-4 items-center justify-center bg-[#1A8DCD] backdrop-blur-md bg-opacity-10  w-full"
+            : "flex flex-col absolute -left-full gap-3 p-4 items-center justify-center bg-[#1A8DCD]  w-full"
+        }
+      >
+        <Link href="/">Home</Link>
+        <Link href="/">About</Link>
+        <Link href="/">Appointment</Link>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <main className="flex  flex-col items-center justify-between p-24">
+   this is home page
+    </main>
+  );
+}
