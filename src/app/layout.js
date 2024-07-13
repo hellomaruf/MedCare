141aa46f3a3d95382b06fbe783a558387e4844c5
maruf-ahmed <marufahamed1110@gmail.com>
2024-07-13 20:58:55 +0600

    Added footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import AuthProvider from "./services/AuthProvider";
+import Footer from "./components/Footer";
 
 const poppins = Poppins({weight:['100','200','300','400','500','600','700','800'], subsets: ["latin"] });
 
@@ -18,7 +19,8 @@ export default function RootLayout({ children }) {
 
         <AuthProvider>
         <Navbar/>
-        {children}
+          {children}
+          <Footer/>
         </AuthProvider>
       </body>
     </html>
